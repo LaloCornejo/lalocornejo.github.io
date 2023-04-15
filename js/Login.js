5b567b3a56f427e2ecfa106c984c2d7c62602a60
L Æ L Ö <128832214+LaloCornejo@users.noreply.github.com>
2023-04-14 23:12:55 -0600

    Add files via upload

---
+++ js/Login.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const btnPopup = document.Login.LoginquerySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+loginLink.addEventListener("click", ()=> {
+    wrapper.classList.remove("active");
+});
+btnPopup.addEventListener("click", ()=> {
+    wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", ()=> {
+    wrapper.classList.remove("active-popup");
+});
