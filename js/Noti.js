c0448291fd1695cf3a36792434aad62929be6429
Lalo Conejo <lalocornejoofficial@gmail.com>
2023-12-24 15:56:36 -0600

    Fix opacity and background gradient in Noti.js

--- js/Noti.js
+++ js/Noti.js
@@ -11,13 +11,13 @@ export function Noti(message, type = "success", bool = false) {
     style: {
       "border-radius": "10px",
       "margin-top": type == "success" ? "200px" : "110px",
-      opacity: ".7",
+      opacity: "1",
       "font-family": "JetBrains Mono",
       "font-size": bool ? "1.5vh" : "1.8vw",
       "@media screen and (max-width: 900px)": {
         "font-size": "2vw",
       },
-      background: type == "success" ? "linear-gradient(45deg, rgba(0,24,126,1) 0%, rgba(255,0,241,1) 77%)" : "linear-gradient(75deg, rgba(255,0,0,0.7) 0%, rgba(52,31,0,0.7) 100%)", // Add a colon after the property name "background"
+      background: type == "success" ? "linear-gradient(45deg, rgba(0,24,126,1) 0%, rgba(255,0,241,1) 100%)" : "linear-gradient(45deg, rgba(255,0,0, 1) 20%, rgba(255,60,0, 1) 100%)", // Add a colon after the property name "background"
       "box-shadow": "0 0 80px" + (type == "success" ? "#ae00ff" : "#ff0000"),
     },
     onClick: function () {}, // Callback after click
